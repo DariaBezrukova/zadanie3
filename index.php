89739a5c04923dcdcfbb799aef4cdd965f05be69
DariaBezrukova <90443596+DariaBezrukova@users.noreply.github.com>
2022-05-04 20:11:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,17 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
 
-// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
-// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
+ 
     if (!empty($_GET['save'])) {
-        // Если есть параметр save, то выводим сообщение пользователю.
+
         print('Спасибо, результаты сохранены.');
     }
-    // Включаем содержимое файла form.php.
     include('form.php');
-    // Завершаем работу скрипта.
+
     exit();
 }
-// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
-// Проверяем ошибки.
+
 $errors = FALSE;
 if (empty($_POST['name'])) {
     print('Заполни имя.<br/>');
@@ -51,11 +47,11 @@ if (empty($_POST['policy'])) {
 }
 
 if ($errors) {
-    // При наличии ошибок завершаем работу скрипта.
+   
     exit();
 }
 
-// Сохранение в базу данных.
+
 $name = $_POST['name'];
 $email = $_POST['email'];
 $date = $_POST['date'];
@@ -65,11 +61,11 @@ $bio = $_POST['bio'];
 $policy = $_POST['policy'];
 $powers = implode(',', $_POST['select']);
 
-$user = 'u47502';
-$pass = '8701243';
+$user = 'u47528';
+$pass = '9694962';
 $db = new PDO('mysql:host=localhost;dbname=u47502', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
 
-// Подготовленный запрос. Не именованные метки.
+
 try {
     $stmt = $db->prepare("INSERT INTO clients SET users = ?, email = ?, date = ?, gender = ?, limbs = ?, bio = ?, policy = ?");
     $stmt->execute(array($name, $email, $date, $gender, $limbs, $bio, $policy));
@@ -82,9 +78,5 @@ try {
     exit();
 }
 
-//  stmt - это "дескриптор состояния".
 
-// Делаем перенаправление.
-// Если запись не сохраняется, но ошибок не видно, то можно закомментировать эту строку чтобы увидеть ошибку.
-// Если ошибок при этом не видно, то необходимо настроить параметр display_errors для PHP.
 header('Location: ?save=1');
